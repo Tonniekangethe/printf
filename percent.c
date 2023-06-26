32f1a5212e70f362bae3ee7cad2a556210c6db00
Tonniekangethe <tonniekamau64@gmail.com>
2023-06-26 16:36:01 -0700

    percent

---
+++ percent.c
@@ -0,0 +1,13 @@
+#iclude "main.h"
+
+/**
+ * print_cent - print the percentage sign
+ * @index: parameter
+ *
+ * Return: 1
+ */
+int print_cent(__attribute__((unused)) va_list index)
+{
+	_putchar('%');
+	return (1);
+}
