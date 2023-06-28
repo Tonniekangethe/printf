2bf27eee2b64a600d4179e1d508760e559eedacf
Leac1m <obemichael10@gmail.com>
2023-06-28 01:36:41 +0100

    project printf

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i)
+ * @f: type pointer to function for the conversion specifier.
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+
+int print_number(va_list index);
+int _strlen(char *s);
+int _strlenc(const char *s);
+int print_cent(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int prt_sting(va_list val);
+
+
+#endif
---
+++ percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_cent - print the percentage sign
+ *
+ * Return: 1
+ */
+int print_cent(void)
+{
+	_putchar(37);
+	return (1);
+}
